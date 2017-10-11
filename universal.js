8c5dce890f1e72ffeef5605920119a20cf0d6405
cyrussmith <cyrussmith@163.com>
2017-10-11 16:09:08 +0800

    提交源代码

---
+++ universal.js
@@ -0,0 +1,17 @@
+﻿function getUrlVars() {
+    var vars = new Object();
+    var hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    //var hashes = window.search.substring(1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars[hash[0]] = unescape(hash[1]);
+    }
+    return vars;
+}
+function getUrlVar(name) {
+    var vals = getUrlVars()[name];
+    if (vals)
+        return vals;
+    return "";
+}
